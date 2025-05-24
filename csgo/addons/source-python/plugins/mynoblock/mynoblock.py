8cdb6bcfe458410ef6871679ddc24224e0f5e3a8
anominy <140955338+anominy@users.noreply.github.com>
2025-05-24 14:19:03 +0300

    Make players with no block enabled transparent

--- csgo/addons/source-python/plugins/mynoblock/mynoblock.py
+++ csgo/addons/source-python/plugins/mynoblock/mynoblock.py
@@ -19,14 +19,40 @@ from typing import Final
 from events import Event, GameEvent
 from messages import SayText2
 from players.entity import Player
+from entities.entity import Entity
 from commands.typed import TypedSayCommand, TypedClientCommand, CommandInfo
 from commands import CommandReturn
+from listeners import OnClientDisconnect
+from colors import Color
+
+
+_players_disable: Final[list[int]] = []
+
+
+@OnClientDisconnect
+def on_client_disconnect(index: int) -> None:
+    global _players_disable
+
+    try:
+        _players_disable.remove(index)
+    except ValueError:
+        pass
 
 
 @Event('player_spawn')
 def on_player_spawn(event: GameEvent) -> None:
+    global _players_disable
+
     player: Final[Player] = Player.from_userid(int(event['userid']))
-    player.set_noblock(True)
+    player_index: Final[int] = player.index
+
+    player_block: Final[bool] = player_index not in _players_disable
+    if player_block:
+        player.color = Color(255, 255, 255, 200)
+    else:
+        player.color = Color(255, 255, 255, 255)
+
+    player.set_noblock(player_block)
 
 
 @TypedSayCommand('!noblock')
@@ -38,9 +64,22 @@ def on_player_spawn(event: GameEvent) -> None:
 @TypedClientCommand('sm_block')
 @TypedClientCommand('sp_block')
 def on_no_block_cmd(info: CommandInfo) -> CommandReturn:
+    global _players_disable
+
     player: Final[Player] = Player(info.index)
+    player_index: Final[int] = player.index
     player_name: Final[str] = player.name
+
     player_block: Final[bool] = not player.get_noblock()
+    if player_block:
+        try:
+            _players_disable.remove(player_index)
+        except ValueError:
+            pass
+        player.color = Color(255, 255, 255, 200)
+    else:
+        _players_disable.append(player_index)
+        player.color = Color(255, 255, 255, 255)
 
     player.set_noblock(player_block)
     SayText2('\x03>^< \x08| \x09' + player_name + '\x08 is ' \
